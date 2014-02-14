80047aaaacf2831415d983f6d806aa8ba513ae65
Kenny Song <kenny.ysong@gmail.com>
2014-02-15 01:13:18 +0400

    facebook friends list

--- client/main.js
+++ client/main.js
@@ -5,10 +5,22 @@ Template.hello.greeting = function () {
 Template.hello.events({
 	'click #facebook-login' : function () {
 		Meteor.loginWithFacebook({
-			requestPermissions: ['email', 'user_location']
+			requestPermissions: ['email', 'user_location', 'user_friends', 'read_friendlists']
 		}, function (err) {
-			if (err)
+			if (err) {
 				Session.set('errorMessage', err.reason || 'Unknown error');
+			}
 		});
 	}
+});
+
+Meteor.autorun(function() {
+  if (Meteor.user()) {
+    // do something when they've just logged in.
+    Meteor.call('FindFriends', function(error, friends) {  
+		id = Meteor.user()._id;
+		Meteor.users.update(id, {$set: {'BadgeIDs': [], 'Level': 1, 'MathWin': 0, 'MathLose': 0, 'ReadingWin': 0, 'ReadingLose': 0, 'WritingWin': 0, 'WritingLose': 0, 'Friends':friends}});
+    });
+    
+  }
 });
