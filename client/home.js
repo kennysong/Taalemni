8544272c8b0aef7418566492e82809160d5ba86b
asyriqueat <asyriue@gmail.com>
2014-02-15 09:34:09 +0400

    Got Publishing of Custom fields working

--- client/home.js
+++ client/home.js
@@ -12,13 +12,13 @@ Template.home.events({
 			}
 			console.log(Meteor.user())
 
-			Meteor.call('FindFriends', function(error, friends) {  
-				id = Meteor.user()._id;
-				console.log("Logged in: " + id)
-				Meteor.users.update(id, {$set: {'BadgeIDs': [], 'Level': 1, 'MathWin': 0, 'MathLose': 0, 
-					'ReadingWin': 0, 'ReadingLose': 0, 'WritingWin': 0, 'WritingLose': 0, 'Friends':friends['data']}});
+			Meteor.call('FindFriends', function(error) {  
+				console.log(error);
 		    });
-
+			Deps.autorun(function(){
+				Meteor.subscribe("userData");
+				console.log("Subscribing!");
+			});
 		});
 	}
 });
