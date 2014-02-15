d9e4b0a8db38adb980cf1ed2ac6517001e07e888
talhajaved <talhajaved1994@gmail.com>
2014-02-15 18:20:13 +0400

    Added UsersHistory to model.js

--- client/dashboard.js
+++ client/dashboard.js
@@ -5,3 +5,11 @@ Template.dashboard.UserPic = function () {
 	return "https://graph.facebook.com/"+Meteor.user().services.facebook.username +"/picture?width=150&height=150";
 }
 
+
+//UsersHistory = new Meteor.Collection("users_history")
+
+
+Template.dashboard.UserGameData = function () {
+	return Meteor.userId()
+
+};
--- client/leaderboard.js
+++ client/leaderboard.js
@@ -2,7 +2,6 @@ Template.leaderboard.heading = function () {
 	return "Leaderboard";
 };
 
-Users = new Meteor.Collection("users_history")
 
 Template.leaderboard.users = function () {
 	return Users.find({}, {sort: {Level: -1}});
