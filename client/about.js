33e5af77bda98a53843a78d0d0b55ca014bed813
Beatrice Ionascu <bi305@nyu.edu>
2014-02-15 14:26:59 +0400

    Populated the about, made a small style change to home

---
+++ client/about.js
@@ -0,0 +1,9 @@
+Template.about.heading = function () {
+	return "What is SATMatch?";
+};
+
+Template.about.text = function () {
+	return "A multiplayer, fast-paced game using historical SAT questions \
+	using gamification techniques to make it competitive, fun, and addictive.";
+};
+
