6978bd383e74e84221d2f0b72e8c6cfff96188eb
asyriqueat <asyriue@gmail.com>
2014-02-15 05:57:38 +0400

    Buttons function bindings implemented

--- client/popup.js
+++ client/popup.js
@@ -1,3 +1,18 @@
 Template.popup.SelfPic = function () {
 	return "https://graph.facebook.com/"+Meteor.user().services.facebook.username +"/picture?width=200&height=200";
 };
+
+Template.popup.events({
+	'click #with-friends' : function(){
+		console.log("clicked friends!")
+		/*Implement switch to friends list here*/
+
+	},
+
+	'click #no-friends' : function(){
+		console.log("no friends")
+		/*Implement global search here*/
+	}
+});
+
+Template.popup.friendslist
--- client/router.js
+++ client/router.js
@@ -1,11 +1,11 @@
 Meteor.Router.add({
   '/': 'home',
   '/about': 'about',
+  '/popup': 'popup',
   '/game/:id': function(id) {
     Session.set('GameID', id);
     return 'game'
   },
   '/dashboard':'dashboard',
-  '*': 'error',
-  '/match': 'popup',
+  '*': 'error'
 });
